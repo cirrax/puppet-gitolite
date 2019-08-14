992c5cb845b03eebb6564ddb742287b4e8f5592a
janit42 <53855472+janit42@users.noreply.github.com>
2019-08-14 22:27:48 +0200

    repo.pp fixed a few typos

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,6 +1,6 @@
 # 
-# this resources defines a git repository
-# you also can use it to define values for a group of
+# this resources define a git repository
+# you also can use them to define values for a group of
 # repositories (by using an @ in front of the group name)
 #
 # @param repos
@@ -20,7 +20,7 @@
 #   additional configs, merged with configs
 # @param groups
 #   an array of groups to append the repos to
-#   the group names can be prefix with an @ sign
+#   the group names can be prefixed with an @ sign
 #   (if they are puppet take care of)
 # @param order
 #   string, to order the repos
@@ -47,7 +47,7 @@
 #   will update the master branch, all tags and all branches stable/*
 #   from the remote location mentioned in url.
 #
-#   To remove an upstrem you can set ensure to 'absent'
+#   To remove an upstream you can set ensure to 'absent'
 #   You can set more than one repo to sync from, but it's up to you
 #   to ensure that no conflicts occur !
 #   
